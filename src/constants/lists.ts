930e20431129d7c990c71c019748791917b26252
Orest <830847+otrsk@users.noreply.github.com>
2022-08-29 18:49:35 +0200

    update list path; don't auto-open browser

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,4 +1,4 @@
 // the Uniswap Default token list lives here
 export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
 
-export const DEFAULT_LIST_OF_LISTS: string[] = [window.location.origin + '/token-list.json']
+export const DEFAULT_LIST_OF_LISTS: string[] = [window.location.origin + '/swap/token-list.json']
