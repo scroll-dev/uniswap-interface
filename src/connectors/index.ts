2e4d7df673e56f50e93263e5224c7a97e48d110c
zhengqi zhang <770166635@qq.com>
2022-09-23 14:33:46 +0800

    change supportedChainIds

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -27,8 +27,8 @@ export function getNetworkLibrary(): Web3Provider {
 }
 
 export const injected = new InjectedConnector({
-         supportedChainIds: [534351, 534352, 534353, 534354, 5343541]
-       })
+  supportedChainIds: [534354]
+})
 
 // mainnet only
 export const walletconnect = new WalletConnectConnector({
