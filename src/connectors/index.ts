711c1669e288e6d27f72529fddbdd2bf6ac22552
zhengqi zhang <770166635@qq.com>
2022-09-23 15:12:56 +0800

    set supportedChainIds

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -27,7 +27,7 @@ export function getNetworkLibrary(): Web3Provider {
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [534354]
+  supportedChainIds: [+(process.env.REACT_APP_CHAIN_ID as string)]
 })
 
 // mainnet only
